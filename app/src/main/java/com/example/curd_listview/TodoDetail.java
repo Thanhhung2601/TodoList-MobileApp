6ea1f1b2d2bfae7c63bb4418aa5313323552f225
hung <anhkp222@gmail.com>
2022-09-11 21:28:15 +0700

    Done

---
+++ app/src/main/java/com/example/curd_listview/TodoDetail.java
@@ -0,0 +1,21 @@
+package com.example.curd_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class TodoDetail extends AppCompatActivity {
+    TextView textViewTitle ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_detail);
+        Todo todo = (Todo) getIntent().getSerializableExtra("todo");
+        Toast.makeText(this, todo.getDescription(), Toast.LENGTH_SHORT).show();
+        textViewTitle = findViewById(R.id.textView7);
+        textViewTitle.setText(todo.getTitle());
+    }
+}
